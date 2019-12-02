22af8c8a74cf20979aa6bbe7456d99c13e8711f5
Elizabeth Doyle <lizdoyle1121@gmail.com>
2019-12-02 01:13:13 -0500

    ran migrations

--- data/migrations/20191202010715_add_tasks.js
+++ data/migrations/20191202010715_add_tasks.js
@@ -1,6 +1,20 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('tasks', table => {
+      table.increments();
+      table.string('desctiption', 255).notNullable();
+      table.string('notes', 255);
+      table.boolean('completed');
+      table
+          .integer('project_id')
+          .unsigned()
+          .references('id')
+          .inTable('projects')
+          .onDelete('RESTRICT')
+          .onUpdate('CASCADE');
+
+    table.timestamps(true, true);
+  })
 };
 
 exports.down = function(knex) {
